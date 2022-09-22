60095f6346dc8bbd1a1ab3e01d24aad83f2af8b1
dlaynes-gl <88404240+dlaynes-gl@users.noreply.github.com>
2022-09-22 12:18:58 -0500

    Update basic dashboard TDD tests

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -4,4 +4,20 @@ test('Dashboard page is available', async ({ page, baseURL }) => {
     await page.goto(baseURL + '/');
 
     await expect(page).toHaveTitle(/Pokemon Angular/);
+
+    const search = page.locator('[data-id="search"]')
+    console.log("Search component", search)
+    expect(search).toHaveCount(1)
+
+    const list = page.locator('[data-id="pokemon-list"]')
+    console.log("List component", list)
+    expect(list).toHaveCount(1)
+
+    const detail = page.locator('[data-id="pokemon-detail"]')
+    console.log("Detail component", detail)
+    expect(detail).toHaveCount(1)
+
+    const navigation = page.locator('[data-id="navigation"]')
+    console.log("Navigation component", navigation)
+    expect(navigation).toHaveCount(1)
 })
