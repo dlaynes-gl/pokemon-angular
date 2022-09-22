6f3e86d305c009cee5148b28fc0caf9b43d69b57
dlaynes-gl <88404240+dlaynes-gl@users.noreply.github.com>
2022-09-22 11:34:36 -0500

    Sync to github

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('Dashboard page is available', async ({ page, baseURL }) => {
     await page.goto(baseURL + '/');
 
-    await expect(page).toHaveTitle(/Dashboard/);
+    await expect(page).toHaveTitle(/Pokemon Angular/);
 })
